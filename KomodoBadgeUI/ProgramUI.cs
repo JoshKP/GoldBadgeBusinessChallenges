da395d24bc6d193a96f05687e0b6b44a10aeea76
Josh Piersimoni <68341491+JoshKP@users.noreply.github.com>
2020-10-28 12:41:42 -0400

    built c3 UI

---
+++ KomodoBadgeUI/ProgramUI.cs
@@ -0,0 +1,14 @@
+﻿using Badge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBadgeUI
+{
+    class ProgramUI
+    {
+        private readonly BadgeRepo _badgeRepo = new BadgeRepo();
+    }
+}
