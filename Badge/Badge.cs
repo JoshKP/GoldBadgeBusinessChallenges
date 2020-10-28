0478b8850954a057f33b9cdcd81a08e585c0f046
Josh Piersimoni <68341491+JoshKP@users.noreply.github.com>
2020-10-28 11:45:05 -0400

    build badge repo for challenge 3

---
+++ Badge/Badge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badge
+{
+    public class Badge
+    {
+        public Badge() { }
+
+        public Badge(int id, List<string> doors)
+        {
+            BadgeID = id;
+            DoorAccess = doors;
+        }
+        public int BadgeID { get; set;}
+
+        public List<string> DoorAccess { get; set;}
+    }
+}
